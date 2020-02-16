45d3d9c976d6258ebad2d9dc895d6f6e057b99dd
Keeqlin <rhythmbboy@gmail.com>
2020-02-16 11:21:40 +0800

    send mails separately

--- sendgmail.py
+++ sendgmail.py
@@ -25,33 +25,33 @@ email_Info(email_json)
 
 sender = email_json['User']['sender']
 passwd = email_json['User']['passed']
+
+smtp = smtplib.SMTP("smtp.gmail.com:587")
+smtp.ehlo()
+smtp.starttls()
+smtp.login(sender, passwd)
 receivers = email_json['Receivers']
  
 emails = receivers.split(',')
-msg = MIMEMultipart()
-msg['Subject'] = email_json['Subject']
-msg['From'] = sender
-msg['To'] = receivers
+for email in emails:
+    msg = MIMEMultipart()
+    msg['Subject'] = email_json['Subject']
+    msg['From'] = sender
+    msg['To'] = email
 
-# load content.txt
-msg.preamble = 'Multipart massage.\n'
-content = MIMEText(open(email_json['Content']).read())
-msg.attach(content)
-
-# load attachments
-attachs = email_json['Attachment'].split(',')
-for attach in attachs:
-    print(attach)
-    part = MIMEApplication(open(attach,"rb").read())
-    part.add_header('Content-Disposition', 'attachment', filename=attach)
+    # load content.txt
+    msg.preamble = 'Multipart massage.\n'
+    part = MIMEText(open(email_json['Content']).read())
     msg.attach(part)
 
+    # load attachments
+    attachs = email_json['Attachment'].split(',')    
+    for attach in attachs:
+        part = MIMEApplication(open(attach,"rb").read())
+        part.add_header('Content-Disposition', 'attachment', filename=attach)
+        msg.attach(part)
+ 
+    smtp.sendmail(msg['From'], email , msg.as_string())
 
-if len(emails) > 0:
-    smtp = smtplib.SMTP("smtp.gmail.com:587")
-    smtp.ehlo()
-    smtp.starttls()
-    smtp.login(sender, passwd)
-    smtp.sendmail(msg['From'], emails , msg.as_string())
-    print ('Send mails to',msg['To'])
-    
+
+print ('Send mails to',receivers)
